58c5d1e0004e3b2a8e8f2437f0c4fe86a9223f7b
KerosenoDev <franciscorodriguezgar@gmail.com>
2018-02-13 21:36:32 +0100

    Añadido método para solicitar nick, y correcciones varias

--- src/com/frassenger/modelo/Cliente.java
+++ src/com/frassenger/modelo/Cliente.java
@@ -16,12 +16,12 @@ import com.frassenger.vista.PanelCliente;
  * @author Francisco Rodríguez García
  */
 public class Cliente {
+	private static final int NOMBRE_USUARIO_MAXIMO = 15;
+	//=====================================================================================================
+	// CONSTANTES
+	//=====================================================================================================
 	private static final int PUESRTO_REGISTRADO_MAXIMO = 49151;
 	private static final int PUERTO_REGISTRADO_MINIMO = 1024;
-	//=====================================================================================================
-	// VARIABLES ESTÁTICAS
-	//=====================================================================================================
-	
 
 	//=====================================================================================================
 	// MÉTODO PRINCIPAL
@@ -88,7 +88,7 @@ public class Cliente {
 	//=====================================================================================================
 	/**
 	 * Solicita un host.
-	 * @param ventana 
+	 * @param ventana Ventana en la que se muestra.
 	 * @return Devuelve el host, en caso de que sea válido.
 	 */
 	private static String solicitarHost(JFrame ventana) {
@@ -105,18 +105,19 @@ public class Cliente {
 			do {
 				// Se inicializan los flags.
 				hayError = false;
-				continuar = JOptionPane.CANCEL_OPTION;
+				continuar = JOptionPane.NO_OPTION;
 
 				// Se solicita el host.
-				host = JOptionPane.showInputDialog(ventana, "Introduce la IP del servidor:", "FRASSENGER", JOptionPane.QUESTION_MESSAGE);
+				host = JOptionPane.showInputDialog(ventana, "Introduce la IP del servidor (localhost en local):", "FRASSENGER", JOptionPane.QUESTION_MESSAGE);
 
 				// Si el host no es 'localhost' ni una IP válida, hay un error.
 				if (!host.equalsIgnoreCase("localhost") && !validadorIp.isValid(host)) {
 					hayError = true;
 
 					// Se pregunta si se quiere reintentar.
-					continuar = JOptionPane.showConfirmDialog(ventana, "La IP no es correcta\n¿Quiere introducir otra?", "FRASSENGER", JOptionPane.YES_NO_OPTION, JOptionPane.ERROR_MESSAGE);
+					continuar = JOptionPane.showConfirmDialog(ventana, "La IP no es correcta.\n¿Quiere introducir otra?", "FRASSENGER", JOptionPane.YES_NO_OPTION, JOptionPane.ERROR_MESSAGE);
 				}
+				
 			// Se repite el proceso si hay fallo y se quiere reintentar.
 			} while (hayError && continuar == JOptionPane.OK_OPTION);
 
@@ -135,8 +136,8 @@ public class Cliente {
 
 	/**
 	 * Solicita un puerto.
-	 * @param ventana 
-	 * @return
+	 * @param ventana Ventana en la que se muestra.
+	 * @return Devuelve el puerto, en caso de que sea válido.
 	 */
 	private static int solicitarPuerto(JFrame ventana) {
 		// Variables
@@ -146,8 +147,8 @@ public class Cliente {
 		try {
 			do {
 				// Se inicializan los flags.
-				hayError = true;
-				continuar = JOptionPane.CANCEL_OPTION;
+				hayError = false;
+				continuar = JOptionPane.NO_OPTION;
 				
 				try {
 					// Se solicita el puerto.
@@ -165,7 +166,7 @@ public class Cliente {
 				
 				// Si hay error, se pregunta si se quiere volver a intentar.
 				if (hayError) {
-					continuar = JOptionPane.showConfirmDialog(ventana, "El puerto no es correcto\n¿Quiere introducir otro?", "FRASSENGER", JOptionPane.YES_NO_OPTION, JOptionPane.ERROR_MESSAGE);
+					continuar = JOptionPane.showConfirmDialog(ventana, "El puerto no es correcto.\n¿Quiere introducir otro?", "FRASSENGER", JOptionPane.YES_NO_OPTION, JOptionPane.ERROR_MESSAGE);
 				}
 			
 			// Se repite el proceso si hay error y se quiere volver a intentar.
@@ -186,11 +187,45 @@ public class Cliente {
 
 	/**
 	 * Solicita un nombre de usuario.
-	 * @param ventana 
-	 * @return
+	 * @param ventana Ventana en la que se muestra.
+	 * @return Devuelve el nombre de usuario.
 	 */
 	private static String solicitarNombreUsuario(JFrame ventana) {
-		// TODO Auto-generated method stub
-		return null;
+		// Variables.
+		String nombreUsuario = null;
+		boolean hayError;
+		int continuar;
+		
+		try {
+			do {
+				// Se inicializan los flags.
+				hayError = false;
+				continuar = JOptionPane.NO_OPTION;
+				
+				// Se solicita el nombre de usuario.
+				nombreUsuario = JOptionPane.showInputDialog(ventana, "Introduce el nombre de usuario:", "FRASSENGER", JOptionPane.QUESTION_MESSAGE);
+				
+				// Si el nombre de usuario está vacío o excede el máximo, hay error.
+				if (nombreUsuario.isEmpty() || nombreUsuario.length() > NOMBRE_USUARIO_MAXIMO) {
+					hayError = true;
+					
+					// Se pregunta si se quiere reintentar.
+					continuar = JOptionPane.showConfirmDialog(ventana, "El nombre de usuario no es válido.\n¿Quiere introducir otro?", "FRASSENGER", JOptionPane.YES_NO_OPTION, JOptionPane.ERROR_MESSAGE);
+				}
+				
+			// Si hay error y se quiere volver a intentar, se repite el proceso.
+			} while (hayError && continuar == JOptionPane.OK_OPTION);
+			
+			// Si hay error y no se quiere volver a intentar, se pone un nombre de usuario por defecto.
+			if (hayError && continuar == JOptionPane.NO_OPTION) {
+				nombreUsuario = "anonymous";
+			}
+			
+		// Si se pulsa cancelar en el JOptionPane, salta la excepción, y se sale.
+		} catch (NullPointerException e) {
+			System.exit(0);
+		}
+		
+		return nombreUsuario;
 	}
 }
